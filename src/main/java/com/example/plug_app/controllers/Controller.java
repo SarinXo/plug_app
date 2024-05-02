ea9b34d069e2197298c37d8fdf9528ed75d1e009
EgorovIA <rollovitch.roll@yandex.ru>
2024-05-02 11:32:58 +0300

    add debug mode

--- src/main/java/com/example/plug_app/controllers/Controller.java
+++ src/main/java/com/example/plug_app/controllers/Controller.java
@@ -1,12 +1,14 @@
 package com.example.plug_app.controllers;
 
 import com.example.plug_app.models.Valute;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
 @RestController
+@Slf4j
 public class Controller {
     private final static Valute valute = new Valute(
             BigDecimal.valueOf(13.18),
@@ -15,7 +17,9 @@ public class Controller {
     );
 
     @GetMapping("/exchange-rate")
-    public Valute getExchangeRate(){
+    public Valute getExchangeRate() throws InterruptedException {
+        log.info("getExchangeRate() Was used");
+        Thread.sleep(500);
         if((int)(Math.random() * 2) == 1){
             throw new RuntimeException("fifty - fifty");
         }
